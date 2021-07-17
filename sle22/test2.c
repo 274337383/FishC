69616d5b43a34c38ae1788ecb23c40b714269e66
w274337383 <274337383@qq.com>
2021-07-17 19:14:26 +0800

    first

---
+++ sle22/test2.c
@@ -0,0 +1,13 @@
+#include "stdio.h"
+
+int main() {
+	char str[128];
+	printf("我的域名是:");
+	scanf("%s", str);
+
+	//printf("我的域名是%s\n", str);
+	printf("我的域名是%p\n", str);
+	printf("我的域名是%p\n", &str[0]);
+
+	return 0;
+}
